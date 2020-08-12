f1f1c99d31294266e55e2253278603ad85d6fbcf
Lucas de souza lameira <lucasslameira@gmail.com>
2020-08-12 19:39:39 -0300

    Expo bug

---
+++ mobile/src/pages/GiveClasses/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text, Image, TouchableOpacity} from 'react-native';
+
+import styles from './styles';
+
+function GiveClasses () {
+    return <View></View>
+}
+
+export default GiveClasses;
---
+++ mobile/src/pages/GiveClasses/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet} from 'react-native'
+
+/*
+'Archivo_400Regular' 
+'Archivo_700Bold'
+'Poppins_400Regular'
+'Poppins_600SemiBold'
+*/ 
+const styles = StyleSheet.create({
+
+});
+
+export default styles;
